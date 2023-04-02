b0761ecf07a4078dc8b05d34888b718d856abdec
Pierre-Luc Brunet <fuitad@gmail.com>
2023-04-02 15:21:45 -0400

    Updated config text with warning about using save_dotted_items_as_array

--- config/langscanner.php
+++ config/langscanner.php
@@ -51,6 +51,11 @@ return [
     | array instead of a flat array. Better for reability and management for large
     | json files
     |
+    | Only use this option if your keys are words or slugs like "page_header" or
+    | "page_header.title". If you use full sentences as keys like "I am error.",
+    | the result json file will be broken because the dot will be changed to an
+    | array.
+    |
     */
     'save_dotted_items_as_array' => false
 ];
